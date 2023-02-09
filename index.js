02fffd4fa718e48daff4eb906eac18db115d3101
Andre <andre.sousa40@etec.sp.gov.br>
2023-02-09 12:14:54 -0300

    feat: instalação e conexão com mongodb e rota GET /item finalizada

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const express = require("express");
-const app = express();
+const { MongoClient, CURSOR_FLAGS } = require("mongodb");
 
-app.use(express.json());
+const DB_URL = "mongodb+srv://andre:andre123@cluster0.holxi2u.mongodb.net/?retryWrites=true&w=majority";
+const DB_NAME = "ocean-bancodados-09-02-2023";
 
-app.get("/", function (req, res) {
-    res.send("Hello World");
-});
+async function main() {
+    console.log("Conectando com banco de dados...");
+    const client = await MongoClient.connect(DB_URL);
+    console.log("Banco de dados conectado com sucesso.");
 
-app.get("/oi", function (req, res) {
-    res.send("Olá Mundo");
-});
+    const db = client.db(DB_NAME);
+    const collection = db.collection("itens");
 
-const itens = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
+    const app = express();
 
-app.get("/item", function (req, res) {
-    res.send(itens);
-});
+    app.use(express.json());
 
-app.get("/item/:id", function (req, res) {
-    const id = req.params.id;
-    const item = itens[id - 1];
-    res.send(item);
-});
+    app.get("/", function (req, res) {
+        res.send("Hello World");
+    });
 
-app.post("/item", function (req, res) {
-    const item = req.body;
-    itens.push(item.nome);
-    res.send("Item criado com sucesso");
-});
+    app.get("/oi", function (req, res) {
+        res.send("Olá Mundo");
+    });
 
-app.listen(3000);
+    const itens = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
+
+    app.get("/item", async function (req, res) {
+        const documentos = await collection.find().toArray();
+        res.send(documentos);
+    });
+
+    app.get("/item/:id", function (req, res) {
+        const id = req.params.id;
+        const item = itens[id - 1];
+        res.send(item);
+    });
+
+    app.post("/item", function (req, res) {
+        const item = req.body;
+        itens.push(item.nome);
+        res.send("Item criado com sucesso");
+    });
+
+    app.listen(3000);
+}
+
+main();
