135d15bc03c2f0e89d604aac1bfccf2873b7d8fa
Andre <andre.sousa40@etec.sp.gov.br>
2023-02-17 13:25:50 -0300

    feat: desafio 1 - criação do componente Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <img src="https://hbomax-images.warnermediacdn.com/2022-08/tt.png?host=wme-hbomax-drupal-prod.s3.amazonaws.com" alt="" />
+
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Criar</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -3,14 +3,24 @@ import './ReadAll.css';
 
 const itens = [
     {
-        _id: '1234',
-        nome: 'Rick Sanchez',
-        imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+        "_id": "63e4f9efa8b70e9e3f665f26",
+        "nome": "Rick Sanchez",
+        "imagemUrl": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
     },
     {
-        _id: '1235',
-        nome: 'Morty Smith',
-        imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+        "_id": "63e50974a8b70e9e3f6d9c5c",
+        "nome": "Morty Smith",
+        "imagemUrl": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    },
+    {
+        "_id": "63e50984a8b70e9e3f6da41f",
+        "nome": "Beth Smith",
+        "imagemUrl": "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+    },
+    {
+        "_id": "63ee1e3e20983f240a4c626e",
+        "nome": "Senhor Calça Cagada",
+        "imagemUrl": "https://rickandmortyapi.com/api/character/avatar/244.jpeg"
     }
 ];
 
